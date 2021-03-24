138aaa82099f6bbfdf8b399e8f8fe69ef0931b40
Junior de Oliveira <ellismarjr@hotmail.com>
2021-03-24 15:19:19 -0300

    validade active subscription

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -40,9 +40,14 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   const session = await getSession({ req });
   const { slug } = params;
 
-  // if (!session) {
-
-  // }
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
 
   const prismic = getPrismicClient(req);
 
@@ -61,7 +66,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
 
   return {
     props: {
-      post
+      post,
     }
   }
 }
