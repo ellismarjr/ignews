01f4b86e8c233c097d7a2bc28a8d02149b50e093
Junior de Oliveira <ellismarjr@hotmail.com>
2021-03-24 11:29:24 -0300

    add navigation in the menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,23 @@
+import Link from 'next/link';
+
 import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news"/>
+        <img src="/images/logo.svg" alt="ig.news" />
 
         <nav>
-          <a href="" className={styles.active}>Home</a>
-          <a href="">Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
