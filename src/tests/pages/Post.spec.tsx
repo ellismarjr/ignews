252ec0457e9992af3774e1911b58eb518c3f9b67
Júnior de Oliveira <ellismarjr@hotmail.com>
2022-01-04 15:14:10 -0300

    tests: [Post page]  loads initial data

--- src/tests/pages/Post.spec.tsx
+++ src/tests/pages/Post.spec.tsx
@@ -36,4 +36,40 @@ describe('Post page', () => {
       })
     }));
   });
+
+  it('loads initial data', async () => {
+    const getSessionMocked = mocked(getSession);
+    const getPrismicClientMocked = mocked(getPrismicClient);
+
+    getSessionMocked.mockResolvedValueOnce({
+      activeSubscription: 'fake-active-subscription'
+    } as any);
+
+    getPrismicClientMocked.mockReturnValueOnce({
+      getByUID: jest.fn().mockResolvedValueOnce({
+        data: {
+          title: [{type: 'heading', text: 'My new post'}],	
+          content: [{type: 'paragraph', text: 'Post content'}],
+        },
+        last_publication_date: '04-01-2021'
+      })
+    } as any);
+
+    const response = await getServerSideProps({
+      params: {slug: 'my-new-post'}
+    } as any);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          post: {
+            slug: 'my-new-post',
+            title: 'My new post',
+            content: '<p>Post content</p>',
+            updatedAt: '01 de abril de 2021'
+          }
+        }
+    }));
+
+  });
 });
